6a72458f5acab93ad3a3074ddedfac71a55519d8
Jason Haenlin <jason.haenlin@etu.univ-cotedazur.fr>
2021-05-16 21:44:03 +0200

    fix commonjs warning

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,22 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SimpleStateHolder = exports.createBasicState = exports.createSelector = exports.createAction = exports.stateHolderConfig = exports.StateHolder = void 0;
-const state_holder_1 = require("./state-holder");
+var state_holder_1 = require("./state-holder");
 Object.defineProperty(exports, "StateHolder", { enumerable: true, get: function () { return state_holder_1.StateHolder; } });
 exports.stateHolderConfig = {
     logger: true
@@ -12,26 +27,30 @@ exports.stateHolderConfig = {
  * @param action the action to dispatch
  * @returns a new action to use in the dispatch function of the state instance
  */
-const createAction = (label, action) => ({ label, action });
+var createAction = function (label, action) { return ({ label: label, action: action }); };
 exports.createAction = createAction;
 /**
  * syntactic sugar to create a new selector
  * @param selector the select function
  * @returns a new selector to use with the select$ function of the state instance
  */
-const createSelector = (selector) => (selector);
+var createSelector = function (selector) { return (selector); };
 exports.createSelector = createSelector;
 /**
  * syntactic sugar to create a new basic state holder. Usefull if you do not need to add any other behaviour to it, only dispatching and selecting outside the class is usefull to you.
  * @param initValues init value of the state
  * @returns a new basic state holder
  */
-const createBasicState = (initValues) => {
+var createBasicState = function (initValues) {
     return new SimpleStateHolder(initValues);
 };
 exports.createBasicState = createBasicState;
-class SimpleStateHolder extends state_holder_1.StateHolder {
-    constructor(initValues) { super(initValues); }
-}
+var SimpleStateHolder = /** @class */ (function (_super) {
+    __extends(SimpleStateHolder, _super);
+    function SimpleStateHolder(initValues) {
+        return _super.call(this, initValues) || this;
+    }
+    return SimpleStateHolder;
+}(state_holder_1.StateHolder));
 exports.SimpleStateHolder = SimpleStateHolder;
 //# sourceMappingURL=index.js.map
--- dist/state-holder.js
+++ dist/state-holder.js
@@ -1,15 +1,26 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.StateHolder = void 0;
-const rxjs_1 = require("rxjs");
-const operators_1 = require("rxjs/operators");
-const _1 = require(".");
+var rxjs_1 = require("rxjs");
+var operators_1 = require("rxjs/operators");
+var _1 = require(".");
 /**
  * Add a state holder manager to a class
  * Can be a simple state holder using the function `createBasicState` to create an instance of the stateHolder. You can also extend a class to add a state manager behaviour to it  (like service in Angular <(^^)> )
  * @param T interface represeting the state structure
  */
-class StateHolder {
+var StateHolder = /** @class */ (function () {
     /**
      * Keep the state of the defined model as `T`
      * DevMode is enable in none production and log the model everytime it has been modified
@@ -38,47 +49,60 @@ class StateHolder {
      * state.select$(textSelector).subscribe({next: (v) => console.log(v)});
      * ```
      */
-    constructor(initValues) {
+    function StateHolder(initValues) {
         this._stateHolderSource = new rxjs_1.BehaviorSubject(initValues);
         this._initValue = initValues;
         this._selectorsMap = {};
         this._stateHolder$ = this._stateHolderSource
-            .pipe(operators_1.scan((all, act) => {
+            .pipe(operators_1.scan(function (all, act) {
             return act[0] ? act[0].action(all, act[1]) : all;
         }, initValues), operators_1.shareReplay(1));
         if (_1.stateHolderConfig.logger) {
             this.devMode();
         }
     }
-    /**
-     * Warning : erase all the data and put it back in the original state.
-     * Next state value will match the iniValue defined in the constructor.
-     */
-    get wipe() { return this._initValue; }
-    /**
-     * Retrieve the state Observable of the model
-     */
-    get stateHolder$() { return this._stateHolder$; }
+    Object.defineProperty(StateHolder.prototype, "wipe", {
+        /**
+         * Warning : erase all the data and put it back in the original state.
+         * Next state value will match the iniValue defined in the constructor.
+         */
+        get: function () { return this._initValue; },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(StateHolder.prototype, "stateHolder$", {
+        /**
+         * Retrieve the state Observable of the model
+         */
+        get: function () { return this._stateHolder$; },
+        enumerable: false,
+        configurable: true
+    });
     /**
      * Utility function to clear the corresponding keys values
      * @param state the current state
      * @param keys the keys to be reset to the default value
      * @returns the new state
      */
-    clear(state, ...keys) {
-        const keysCleared = {};
-        keys.forEach(k => this._initValue[k] != null ? keysCleared[k] = this._initValue[k] : null);
-        return Object.assign(Object.assign({}, state), keysCleared);
-    }
+    StateHolder.prototype.clear = function (state) {
+        var _this = this;
+        var keys = [];
+        for (var _i = 1; _i < arguments.length; _i++) {
+            keys[_i - 1] = arguments[_i];
+        }
+        var keysCleared = {};
+        keys.forEach(function (k) { return _this._initValue[k] != null ? keysCleared[k] = _this._initValue[k] : null; });
+        return __assign(__assign({}, state), keysCleared);
+    };
     /**
      * Dispatch a new action to update the state
      * @param actionDef `ActionDef<T, S>` dispatch an action created by `createAction()`
      * @param args the arguments to dispatch to the state to update it
      */
-    dispatch(actionDef, args) {
+    StateHolder.prototype.dispatch = function (actionDef, args) {
         this._lastActionName = actionDef.label;
         this._stateHolderSource.next([actionDef, args]);
-    }
+    };
     /**
      * Select a value from the state
      * The select cache the observable created by the createSelector using the name of it.
@@ -89,29 +113,31 @@ class StateHolder {
      * @param selector `Selector<T, O, I>` define the selector function
      * @returns the observable corresponding to your selector function
      */
-    select$(selector, args) {
-        const cachedObs = this._selectorsMap[selector.name];
+    StateHolder.prototype.select$ = function (selector, args) {
+        var cachedObs = this._selectorsMap[selector.name];
         if (cachedObs) {
             return cachedObs;
         }
         if (args) {
-            const newObs = this._stateHolder$.pipe(operators_1.map(state => selector(state, args)), this.processPipe());
-            this._selectorsMap[selector.name] = newObs;
-            return newObs;
+            var newObs_1 = this._stateHolder$.pipe(operators_1.map(function (state) { return selector(state, args); }), this.processPipe());
+            this._selectorsMap[selector.name] = newObs_1;
+            return newObs_1;
         }
-        const selectorWithoutArgs = selector;
-        const newObs = this._stateHolder$.pipe(operators_1.map(state => selectorWithoutArgs(state)), this.processPipe());
+        var selectorWithoutArgs = selector;
+        var newObs = this._stateHolder$.pipe(operators_1.map(function (state) { return selectorWithoutArgs(state); }), this.processPipe());
         this._selectorsMap[selector.name] = newObs;
         return newObs;
-    }
-    devMode() {
+    };
+    StateHolder.prototype.devMode = function () {
+        var _this = this;
         this._stateHolder$
             .pipe(operators_1.distinctUntilChanged())
-            .subscribe({ next: m => { var _a; return console.log({ action: (_a = this._lastActionName) !== null && _a !== void 0 ? _a : 'initial', state: m }); } });
-    }
-    processPipe() {
-        return rxjs_1.pipe(operators_1.filter(d => d !== null && d !== undefined), operators_1.distinctUntilChanged());
-    }
-}
+            .subscribe({ next: function (m) { var _a; return console.log({ action: (_a = _this._lastActionName) !== null && _a !== void 0 ? _a : 'initial', state: m }); } });
+    };
+    StateHolder.prototype.processPipe = function () {
+        return rxjs_1.pipe(operators_1.filter(function (d) { return d !== null && d !== undefined; }), operators_1.distinctUntilChanged());
+    };
+    return StateHolder;
+}());
 exports.StateHolder = StateHolder;
 //# sourceMappingURL=state-holder.js.map
